fd5b2b21b381772aa6b81137c9ef0ffbcdd56e8c
Yue Sun <757075280@qq.com>
2020-03-05 21:40:04 +0800

    add index

--- src/main/java/life/yurie/community/controller/IndexController.java
+++ src/main/java/life/yurie/community/controller/IndexController.java
@@ -1,21 +1,30 @@
 package life.yurie.community.controller;
 
+import life.yurie.community.dto.QuestionDTO;
+import life.yurie.community.mapper.QuestionMapper;
 import life.yurie.community.mapper.UserMapper;
+import life.yurie.community.model.Question;
 import life.yurie.community.model.User;
+import life.yurie.community.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @Controller
 public class IndexController {
     @Autowired
     private UserMapper userMapper;
 
+    @Autowired
+    private QuestionService questionService;
     @GetMapping("/")
-    public String index(HttpServletRequest request) {
+    public String index(HttpServletRequest request,
+                        Model model) {
         Cookie[] cookies = request.getCookies();
         if (cookies != null && cookies.length != 0) {
             for (Cookie cookie : cookies) {
@@ -29,6 +38,8 @@ public class IndexController {
                 }
             }
         }
+        List<QuestionDTO> questionList = questionService.list();
+        model.addAttribute("questions", questionList);
         return "index";
     }
 }
